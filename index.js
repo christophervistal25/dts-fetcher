cfe8adb673089d062996ecf9ae9a9601ad13a81c
CHRis <christophervistal25@gmail.com>
2021-03-20 00:16:07 +0800

    add entry route & set base url for axios

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const PORT    = process.env.PORT || 3030;
 
 require('events').EventEmitter.defaultMaxListeners = 15;
 
+axios.defaults.baseURL = 'http://192.168.1.2:8000';
+
 // Mysql Database credentials
 const HOST     = 'localhost';
 const USERNAME = 'root';
@@ -21,6 +23,10 @@ let connection = mysql.createConnection({
 
 connection.connect();
 
+app.get('/', (req,res) => {
+    res.send('Hello , World');
+});
+
 app.get('/posts', (req, res) => {
     connection.query('SELECT * FROM dtse0dceaf439f38b60135bf84898cf7951_data_tb', (error, result, fields) => {
         res.setHeader('Content-Type', 'application/json');
@@ -29,7 +35,7 @@ app.get('/posts', (req, res) => {
 });
 
 async function fetchRecord() {
-    const recordResponse = await axios('http://192.168.1.2:8000/services/api/documents/records.php');
+    const recordResponse = await axios('/services/api/documents/records.php');
     let records = await recordResponse;
 
     records.data.map((record) => {
@@ -40,7 +46,7 @@ async function fetchRecord() {
         });
     });
     
-    const historyResponse = await axios('http://192.168.1.2:8000/services/api/documents/history.php');
+    const historyResponse = await axios('/services/api/documents/history.php');
     let result = await historyResponse;
     result.data.map((history) => {
         connection.query("INSERT IGNORE INTO dtsa6e7ffacbf8feb04facb340d62ceddd7_history_logs_tb SET ?", history, (error,  result, fields) => {
