7e65f2b32d39b2c4511cb4767e85fc3b9ccce110
undeadban-emp <undeadban.emp@gmail.com>
2021-04-05 23:04:34 +0800

    seperate business logic

--- index.js
+++ index.js
@@ -1,66 +1,43 @@
-const axios = require("axios");
 let mysql = require("mysql");
+let _ = require("lodash");
+const axios = require("axios");
 const express = require("express");
-let FormData = require("form-data");
-const { report } = require("process");
-const { count } = require("console");
-const { response } = require("express");
-const { toNamespacedPath } = require("path");
 const app = express();
+const path = require("path");
+
+const doc = require("./modules/document.js");
 const PORT = process.env.PORT || 3030;
+const connection = require("./modules/database-connection.js");
 
 require("events").EventEmitter.defaultMaxListeners = 15;
 
-axios.defaults.baseURL = "http://192.168.1.14:8000";
+axios.defaults.baseURL = "http://192.168.0.104:8000/";
 
-  // Mysql Database credentials
-const HOST = "localhost";
-const USERNAME = "root";
-const PASSWORD = "";
-const DB_NAME = "dts_app";
+app.get("/", (_, res) =>
+  res.sendFile(path.join(__dirname + "/public/index.html"))
+);
 
-let connection = mysql.createConnection({
-  host: HOST,
-  user: USERNAME,
-  password: PASSWORD,
-  database: DB_NAME,
-});
+// let requestWebData = async () => {
+//   const recordResponse = await axios("/services/api/documents/records.php");
+//   let documents = await recordResponse;
+//   doc.insertAll(documents.data);
 
-connection.connect();
+//   const historyResponse = await axios("/services/api/documents/history.php");
+//   let histories = await historyResponse;
+//   doc.insertHistories(histories.data);
 
-app.get("/", (req, res) => {
-  res.send("Hello , World");
-});
+//   // This will push to live server.
+//   doc.pushDocuments();
+// };
 
-async function fetchRecord() {
-  //   const recordResponse = await axios("/services/api/documents/records.php");
-  //   let records = await recordResponse;
+let requestWebData = async () => {
+  const recordResponse = await axios("/services/api/documents/records.php");
+  let documents = await recordResponse;
+  doc.insertAll(documents.data);
 
-  //   records.data.map((record) => {
-  //     connection.query(
-  //       "INSERT IGNORE INTO dtse0dceaf439f38b60135bf84898cf7951_data_tb SET ?",
-  //       record,
-  //       (error, result, fields) => {
-  //         if (error) {
-  //           throw new Error("Duplicate entry for document");
-  //         }
-  //       }
-  //     );
-  //   });
-
-  //   const historyResponse = await axios("/services/api/documents/history.php");
-  //   let result = await historyResponse;
-  //   result.data.map((history) => {
-  //     connection.query(
-  //       "INSERT IGNORE INTO dtsa6e7ffacbf8feb04facb340d62ceddd7_history_logs_tb SET ?",
-  //       history,
-  //       (error, result, fields) => {
-  //         if (error) {
-  //           throw new Error(`Duplicate entry for document history`);
-  //         }
-  //       }
-  //     );
-  //   });
+  const historyResponse = await axios("/services/api/documents/history.php");
+  let histories = await historyResponse;
+  doc.insertHistories(histories.data);
 
   // connection.query(
   //   "SELECT * FROM dtse0dceaf439f38b60135bf84898cf7951_data_tb",
@@ -80,128 +57,147 @@ async function fetchRecord() {
   //       .catch((err) => console.log(err));
   //   }
   // );
-    let data = { 
-        'data_datetoday'         : '2021-03-',
-        'data_pr_no'             : '',
-        'data_pr_date'           : '2021-03-23',
-        'data_office'            : 'PGO',
-        'data_claimant'          : 'Joey Garcia',
-        'data_transaction'       : 'no_input',
-        'data_amount'            : 5.00,
-        'data_purpose'           : 3,
-        'data_charge_to'         : 3,
-        'data_current_department': 'PGSO',
-        'data_current_station'   : 1001,
-        'from_local'             : true,
-    };
 
+  let data = {
+    data: [
+      {
+        data_datetoday: "2021-03",
+        data_pr_no: "",
+        data_pr_date: "2021-03-23",
+        data_office: "PGO",
+        data_claimant: "Joey Garcia",
+        data_transaction: "no_input",
+        data_amount: 5.0,
+        data_purpose: 3,
+        data_charge_to: 3,
+        data_current_department: "PGSO",
+        data_current_station: 1001,
+      },
+    ],
+    from_local: true,
+  };
 
-      // axios
-      // .post(`dts_admin_d70c9453e1f41d4624f2937b05819317/c79bdf421714f5087fc34b7c538b6807/transaction/added_btn_particulars.php`,  JSON.parse(JSON.stringify(data)) )
-      // .then((response) => console.log(response.data))
-      // .catch((err) => console.log(err));
+  // axios
+  //   .post(
+  //     `dts_admin_d70c9453e1f41d4624f2937b05819317/c79bdf421714f5087fc34b7c538b6807/transaction/added_btn_particulars.php`,
+  //     JSON.parse(JSON.stringify(data))
+  //   )
+  //   .then((response) => console.log(response.data))
+  //   .catch((err) => console.log(err));
 
-      
-      // axios
-      // .post(`dts_admin_d70c9453e1f41d4624f2937b05819317/c79bdf421714f5087fc34b7c538b6807/transaction/added_btn_history_logs.php`,  JSON.parse(JSON.stringify(data)) )
-      // .then((response) => console.log(response.data))
-      // .catch((err) => console.log(err));
-      
+  // axios
+  //   .post(
+  //     `dts_admin_d70c9453e1f41d4624f2937b05819317/c79bdf421714f5087fc34b7c538b6807/transaction/added_btn_history_logs.php`,
+  //     JSON.parse(JSON.stringify(data))
+  //   )
+  //   .then((response) => console.log(response.data))
+  //   .catch((err) => console.log(err));
 
-      axios
-        .post(`dts_admin_d70c9453e1f41d4624f2937b05819317/c79bdf421714f5087fc34b7c538b6807/transaction/added_btn_data.php`,  JSON.parse(JSON.stringify(data)) )
-        .then((response) => console.log(response.data))
-        .catch((err) => console.log(err));
+  axios
+    .post(
+      `dts_admin_d70c9453e1f41d4624f2937b05819317/c79bdf421714f5087fc34b7c538b6807/transaction/added_btn_data_api.php`,
+      data
+    )
+    .then((response) => console.log(response.data))
+    .catch((err) => console.log(err));
+};
 
-}
+setInterval(requestWebData, 2000);
 
-setInterval(fetchRecord, 2000);
-
-const server = app.listen(PORT, () => console.log(`Server start listening to port ${PORT}`));
-
+const server = app.listen(PORT, () =>
+  console.log(`Server start listening to port ${PORT}`)
+);
 
 const io = require("socket.io")(server, {
   cors: { origin: "*" },
 });
 
 io.on("connection", (socket) => {
-
   socket.on("SEND_PR_DATA", (document) => {
-
     let doc = {
-      dtse0dce_data_reference_no : document.dtse0dce_data_reference_no,
-      dtse0dce_data_pr_no : document.dtse0dce_data_pr_no,
-      dtse0dce_data_pr_date : document.dtse0dce_data_pr_date,
-      dtse0dce_data_office : document.dtse0dce_data_office,
-      dtse0dce_data_claimant : document.dtse0dce_data_claimant,
-      dtse0dce_data_particulars : document.dtse0dce_data_particulars,
-      dtse0dce_data_amount : document.dtse0dce_data_amount,
-      dtse0dce_data_purpose : document.dtse0dce_data_purpose,
-      dtse0dce_data_charge_to : document.dtse0dce_data_charge_to,
-      dtse0dce_data_current_department : document.dtse0dce_data_current_department,
-      dtse0dce_data_current_station : document.dtse0dce_data_current_station,
+      dtse0dce_data_reference_no: document.dtse0dce_data_reference_no,
+      dtse0dce_data_pr_no: document.dtse0dce_data_pr_no,
+      dtse0dce_data_pr_date: document.dtse0dce_data_pr_date,
+      dtse0dce_data_office: document.dtse0dce_data_office,
+      dtse0dce_data_claimant: document.dtse0dce_data_claimant,
+      dtse0dce_data_particulars: document.dtse0dce_data_particulars,
+      dtse0dce_data_amount: document.dtse0dce_data_amount,
+      dtse0dce_data_purpose: document.dtse0dce_data_purpose,
+      dtse0dce_data_charge_to: document.dtse0dce_data_charge_to,
+      dtse0dce_data_current_department:
+        document.dtse0dce_data_current_department,
+      dtse0dce_data_current_station: document.dtse0dce_data_current_station,
     };
 
     let history = {
-      dtsa6e7f_history_logs_user_id : document.dtsa6e7f_history_logs_user_id,
-      dtsa6e7f_history_logs_datetime : document.dtsa6e7f_history_logs_datetime,
-      dtsa6e7f_history_logs_office : document.dtsa6e7f_history_logs_office,
-      dtsa6e7f_history_logs_claimant : document.dtsa6e7f_history_logs_claimant,
-      dtsa6e7f_history_logs_transaction : document.dtsa6e7f_history_logs_transaction,
-      dtsa6e7f_history_logs_data_id : document.dtsa6e7f_history_logs_data_id,
-      dtsa6e7f_history_logs_current_department : document.dtsa6e7f_history_logs_current_department,
-      dtsa6e7f_history_logs_current_station : document.dtsa6e7f_history_logs_current_station,
+      dtsa6e7f_history_logs_user_id: document.dtsa6e7f_history_logs_user_id,
+      dtsa6e7f_history_logs_datetime: document.dtsa6e7f_history_logs_datetime,
+      dtsa6e7f_history_logs_office: document.dtsa6e7f_history_logs_office,
+      dtsa6e7f_history_logs_claimant: document.dtsa6e7f_history_logs_claimant,
+      dtsa6e7f_history_logs_transaction:
+        document.dtsa6e7f_history_logs_transaction,
+      dtsa6e7f_history_logs_data_id: document.dtsa6e7f_history_logs_data_id,
+      dtsa6e7f_history_logs_current_department:
+        document.dtsa6e7f_history_logs_current_department,
+      dtsa6e7f_history_logs_current_station:
+        document.dtsa6e7f_history_logs_current_station,
     };
-    
-    let particulars = {
-      dts_particulars_item_no : document.dts_particulars_item_no,
-      dts_particulars_quantity : document.dts_particulars_quantity,
-      dts_particulars_data_id : document.dts_particulars_data_id,
-      dts_particulars_unit_of_issue : document.dts_particulars_unit_of_issue,
-      dts_particulars_item_description : document.dts_particulars_item_description,
-      dts_particulars_estimated_unit_coast : document.dts_particulars_estimated_unit_coast,
-      dts_particulars_estimated_total_coast : document.dts_particulars_estimated_total_coast,
-    }
-    
-    connection.beginTransaction(function(err) {
-          if (err) { throw err; }
-      
-          // Insertion of document
-          connection.query("INSERT IGNORE INTO dtse0dceaf439f38b60135bf84898cf7951_data_tb SET ?", doc, (error, result, fields) => {
+
+    connection.beginTransaction(function (err) {
+      if (err) {
+        throw err;
+      }
+
+      // Insertion of Document
+      connection.query(
+        "INSERT IGNORE INTO dtse0dceaf439f38b60135bf84898cf7951_data_tb SET ?",
+        doc,
+        (error, result, fields) => {
+          if (error) {
+            throw error;
+          }
+
+          // Insertion of History
+          connection.query(
+            "INSERT IGNORE INTO dtsa6e7ffacbf8feb04facb340d62ceddd7_history_logs_tb SET ?",
+            history,
+            (error, result, fields) => {
               if (error) {
-              throw new Error("Duplicate entry for document");
+                throw error;
               }
 
-              // Insertion of history
-              connection.query("INSERT IGNORE INTO dtsa6e7ffacbf8feb04facb340d62ceddd7_history_logs_tb SET ?", history, (error, result, fields) => {
-                  if (error) {
-                  throw new Error("Duplicate entry for document");
+              // Insertion of Particulars
+              document.particulars.map((particular) => {
+                connection.query(
+                  "INSERT IGNORE INTO dts_particulars_tb SET ?",
+                  particular,
+                  (error, result, fields) => {
+                    if (error) {
+                      throw error;
+                    }
                   }
-
-                  // Insertion of particulars
-                  connection.query("INSERT IGNORE INTO dts_particulars_tb SET ?", particulars, (error, result, fields) => { 
-                      if (error) {
-                      throw new Error("Duplicate entry for document");
-                      }
-                      
-                      // Commit the transaction.
-                      connection.commit(function(err) {
-                          if (err) {
-                              return connection.rollback(function() {
-                                throw err;
-                              });
-                          }
-                          console.log(`New data insert from liason : ${doc.dtse0dce_data_claimant}`)
-                          io.emit('data_passed', { success : true });
-                      });
-                  });
+                );
               });
-        });
+
+              connection.commit(function (err) {
+                if (err) {
+                  return connection.rollback(function () {
+                    throw err;
+                  });
+                }
+
+                console.log(
+                  `New data insert from liason : ${doc.dtse0dce_data_claimant}`
+                );
+
+                io.emit("data_passed", { success: true });
+              });
+            }
+          );
+        }
+      );
     });
-
   });
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
+  socket.on("disconnect", () => console.log("user disconnected"));
 });
